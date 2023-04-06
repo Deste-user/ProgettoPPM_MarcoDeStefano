45f2e612224e7b42e97225d3cbe977bcfc1952ba
marcodestefano <marco.destefano1@stud.unifi.it>
2023-04-06 20:02:08 +0200

    Initial Commit

--- Js/index.js
+++ Js/index.js
@@ -1,17 +1,58 @@
 $(document).ready(function () {
-    $(".SearchInput").focusin(function() {
+
+
+    $(".SearchInput").focusin(function () {
         $(".InputSearchFiles").css("border-bottom", "2px solid red");
     });
-    $(".SearchInput").focusout(function() {
+
+    $(".SearchInput").focusout(function () {
         $(".InputSearchFiles").css("border-bottom", "2px solid grey");
     });
 
-    var altezza_img = $("#ImgCamp").height();
+    let altezza_img = $("#ImgCamp").height();
     $(".SecondRowImage").css("height", altezza_img);
-    var larghezza_img = $(".SecondRowImage").width();
+
+    let larghezza_img = $(".SecondRowImage").width();
     $(".FirstRowImage").width(larghezza_img);
+
+    window.onscroll = function() {scrollFunction(); scrollFunction1(); };
+
+
+
 });
 
-function backToTop() {
-    $("html, body").animate({scrollTop: 0}, 1000);
+function scrollFunction() {
+    let mybutton = $("#Buttontop");
+    if ($(window).scrollTop() > 20 ) {
+        mybutton.css("display", "block")
+    } else {
+        mybutton.css("display", "none")
+    }
 }
+function topFunction() {
+    window.scrollTo(0,0)
+}
+
+function scrollFunction1() {
+    if ($(window).scrollTop() > 80) {
+        $("#nav").css("padding", "0px 40px");
+        $("#todis1").css("display", "none");
+        $("#todis2").css("display", "none");
+        $("#RowToCenter").css("flex-direction", "row-reverse");
+        $("#frh").css("justify-content", "end");
+        $("#logo").css("line-height", "1.4");
+        $("#logo").css("width", "auto");
+    } else {
+        $("#nav").css("padding", "20px 40px");
+        $("#todis1").css("display", "flex");
+        $("#todis2").css("display", "flex");
+        $("#RowToCenter").css("flex-direction", "column");
+        $("#frh").css("justify-content", "space-between");
+        $("#logo").css("line-height", "2");
+        $("#logo").css("width", "15%");
+    }
+};
+
+
+
+
