71a2195ac555b9e8b1249de8fd38ed8c3b145e06
marcodestefano <marco.destefano1@stud.unifi.it>
2023-04-12 20:13:50 +0200

    Initial Commit

--- Js/index.js
+++ Js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+    let larghezza_schermo= $(window).width();
 
     $(".SearchInput").focusin(function () {
         $(".InputSearchFiles").css("border-bottom", "2px solid red");
@@ -10,14 +10,53 @@ $(document).ready(function () {
     });
 
     let altezza_img = $("#ImgCamp").height();
-    $(".SecondRowImage").css("height", altezza_img);
+
+    if(larghezza_schermo <= 1080){
+        $(".AAA").css("height", 600);
+        $(".SecondRowImage").css("height", 300);
+        $(".colimage").css("height", 600 );
+    }else{
+        $(".SecondRowImage").css("height", 186);
+        $(".AAA").css("height", 373);
+        $(".colimage").css("height", altezza_img);
+    }
+
 
     let larghezza_img = $(".SecondRowImage").width();
-    $(".FirstRowImage").width(larghezza_img);
+    $(".colimage").width(larghezza_img);
 
-    window.onscroll = function() {scrollFunction(); scrollFunction1(); };
+    $(window).resize(function (){
+        larghezza_schermo= $(window).width();
+        $(".SearchInput").focusin(function (){
+            $(".InputSearchFiles").css("border-bottom", "2px solid red");
+        });
 
+        $(".SearchInput").focusout(function (){
+            $(".InputSearchFiles").css("border-bottom", "2px solid grey");
+        });
+        let altezza_img = $("#ImgCamp").height();
+        if(larghezza_schermo <= 1080){
+        $(".AAA").css("height", 600);
+        $(".SecondRowImage").css("height", 300);
+        $(".colimage").css("height", 600);
+        }else{
+        $(".AAA").css("height", 373);
+        $(".SecondRowImage").css("height", 186);
+        $(".colimage").css("height", altezza_img);
+        }
 
+        let larghezza_img = $(".SecondRowImage").width();
+        $(".colimage").width(larghezza_img);
+
+    });
+
+    window.onscroll = function() {
+        let larg= $(window).width();
+        scrollFunction();
+        if(larg > 1080){
+        scrollFunction1();
+        }
+        };
 
 });
 
@@ -40,7 +79,10 @@ function scrollFunction1() {
         $("#todis2").css("display", "none");
         $("#RowToCenter").css("flex-direction", "row-reverse");
         $(".SecondRowHeader").css("margin-top", "0px");
+        $(".SecondRowHeader").css("margin-left","-30px");
+        $(".SecondRowHeader").css("margin-right","20px");
         $("#frh").css("justify-content", "end");
+        $(".IconPlus").css("margin-left", "30px");
         $("#logo").css("line-height", "1.2");
         $("#logo").css("width", "13%");
     } else {
@@ -49,6 +91,9 @@ function scrollFunction1() {
         $("#todis2").css("display", "flex");
         $("#RowToCenter").css("flex-direction", "column");
         $(".SecondRowHeader").css("margin-top", "45px");
+        $(".SecondRowHeader").css("margin-left","0px");
+        $(".SecondRowHeader").css("margin-right","0px");
+        $(".IconPlus").css("margin-left", "30px");
         $("#frh").css("justify-content", "space-between");
         $("#logo").css("line-height", "2");
         $("#logo").css("width", "17%");
